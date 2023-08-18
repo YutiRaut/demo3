8993837973e431081656e3988bdc20df5a3f816d
rarti <yutiraut2002@gmail.com>
2023-08-18 11:48:37 +0530

    Initial commit

---
+++ src/main/java/TestDemo.java
@@ -0,0 +1,10 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("Add is"+add(5,5));
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+}
---
+++ src/test/java/demoTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class demoTest {
+
+    @Test
+    public  void  testAdd() {
+
+int expected=TestDemo.add(2,2);
+assertEquals(expected,4);
+    }
+
+
+
+
+}
